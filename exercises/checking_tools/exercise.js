a23114306becb4ea978aa50423860e37485185fe
Dima Belitsky <dmitry.belitsky@gmail.com>
2014-08-01 16:02:38 +0400

    todo: checking available tools task

--- exercises/checking_tools/exercise.js
+++ exercises/checking_tools/exercise.js
@@ -6,13 +6,27 @@ var path = require('path'),
     toolVersions = [];
 
 exercise.requireSubmission = false;
+exercise.addProcessor(checkToolsDirs);
 
-tools.map(function(tool) {
-    var currentCommand = tool + ' --version';
-    var currentToolVersion = exec(currentCommand, function(err, stdOut, stdErr) {
-        toolVersions.push(stdOut);
-        console.log(toolVersions);
+var failBrokenTool = function() {
+    exercise.emit('fail', 'You should install needed tools: git, node and Yeoman');
+    callback(null, false);
+};
+
+var checkToolsDirs = function(callback) {
+    tools.map(function(tool) {
+        var currentCommand = tool + ' --version';
+        var currentToolVersion = exec(currentCommand, function(err, stdOut, stdErr) {
+            if (err) {
+                return failBrokenTool();
+            }
+
+            toolVersions.push(stdOut);
+            console.log(toolVersions);
+            callback(null, true);
+        });
     });
-});
+};
 
+module.exports.checkToolsDirs = checkToolsDirs;
 module.exports = exercise;
