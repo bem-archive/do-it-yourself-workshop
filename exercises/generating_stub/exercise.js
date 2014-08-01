262bb60817115ebceb203523de1f0edebdd9900f
Dima Belitsky <dmitry.belitsky@gmail.com>
2014-08-01 16:43:21 +0400

    added generator results checking

--- exercises/generating_stub/exercise.js
+++ exercises/generating_stub/exercise.js
@@ -1,18 +1,33 @@
 var exercise = require('workshopper-exercise')(),
     path = require('path'),
-    fs = require('fs');
+    fs = require('fs'),
+    _this;
+
+var checkPaths = [
+    'libs/bem-components/',
+    'libs/bem-core/',
+    'node_modules/bem/'
+];
 
 exercise.requireSubmission = false;
 
-var checkPaths = [
-    'libs/bem-components',
-    'libs/bem-core',
-    'node_modules/bem'
-];
-
-checkPaths.map(function(pathItem) {
-    var currentPath = path.join(__dirname + '../../../' + '/bem-stub/' + pathItem);
-    console.log(currentPath);
+exercise.addVerifyProcessor(function (callback) {
+    _this = this;
+    function failBadPath() {
+        exercise.emit('fail', 'You should install generator-bem-stub with needed params.')
+    }
+    checkPaths.map(function(pathItem) {
+        var currentPath = path.join(__dirname + '../../..' + '/bfs-stub/' + pathItem);
+        console.log(currentPath);
+        fs.stat(currentPath, function(err, stat) {
+            if (err) return failBadPath();
+            if (!stat.isDirectory()) return failBadPath();
+            exercise.emit('pass', 'Libraries on the places');
+            callback(null, true);
+        });
+    });
 });
+
+
 
 module.exports = exercise;
