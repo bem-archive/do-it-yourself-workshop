51f4658d2fad81e8df61af2314d6612dd500fd83
Dima Belitsky <dmitry.belitsky@gmail.com>
2014-08-01 16:26:30 +0400

    fixed tools checking exercise

--- exercises/checking_tools/exercise.js
+++ exercises/checking_tools/exercise.js
@@ -3,30 +3,33 @@ var exercise = require('workshopper-exercise')();
 var path = require('path'),
     exec = require('child_process').exec,
     tools = ['node', 'git', 'yo'],
-    toolVersions = [];
+    toolVersions = [],
+    _this;
 
 exercise.requireSubmission = false;
-exercise.addProcessor(checkToolsDirs);
 
-var failBrokenTool = function() {
-    exercise.emit('fail', 'You should install needed tools: git, node and Yeoman');
-    callback(null, false);
-};
+exercise.addVerifyProcessor(function (callback) {
+    _this = this;
+    function failBrokenTool() {
+        exercise.emit('fail', 'You should install needed tools: git, node and Yeoman');
+        callback(null, false);
+    }
 
-var checkToolsDirs = function(callback) {
     tools.map(function(tool) {
         var currentCommand = tool + ' --version';
-        var currentToolVersion = exec(currentCommand, function(err, stdOut, stdErr) {
+        exec(currentCommand, function(err, stdOut, stdErr) {
             if (err) {
+                _this.emit('fail', 'Needed tools not found');
                 return failBrokenTool();
             }
 
             toolVersions.push(stdOut);
-            console.log(toolVersions);
+            _this.emit('pass', 'All needed tools installed');
+
             callback(null, true);
         });
     });
-};
+});
 
-module.exports.checkToolsDirs = checkToolsDirs;
+
 module.exports = exercise;
